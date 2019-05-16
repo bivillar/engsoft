68cb9d08d316e3c9473dfede6d3509141ef1ed72
bivillar <bianca.villar@dbsoft.com.br>
2019-05-16 09:45:47 -0300

    Mockup cards

--- src/components/pages/Allslides.jsx
+++ src/components/pages/Allslides.jsx
@@ -3,6 +3,8 @@ import Title from "../Title";
 import Slidecard from "../SlideCard";
 import { CardDeck, Card } from "react-bootstrap";
 import Aula1 from "../../images/Aula1.jpg";
+import Aula2 from "../../images/Aula2.jpg";
+import Aula3 from "../../images/Aula3.jpg";
 
 class allslides extends Component {
   state = {
@@ -13,6 +15,16 @@ class allslides extends Component {
         name: "Aula 1",
         image: Aula1,
         num: 1
+      },
+      {
+        name: "Aula 2",
+        image: Aula2,
+        num: 2
+      },
+      {
+        name: "Aula 3",
+        image: Aula3,
+        num: 3
       }
     ]
   };
@@ -22,25 +34,9 @@ class allslides extends Component {
       <React.Fragment>
         <Title title={this.state.title} subtitle={this.state.subtitle} />
         <CardDeck className="container">
-          {/* <Card>
-            <Card.Img variant="top" src={this.state.slides[0].image} />
-            <Card.Body>
-              <Card.Title>{this.state.slides[0].name}</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card> */}
-          <Slidecard
-            image={this.state.slides[0].image}
-            name={this.state.slides[0].name}
-            num={this.state.slides[0].num}
-          />
+          {this.state.slides.map(slide => (
+            <Slidecard image={slide.image} name={slide.name} num={slide.num} />
+          ))}
         </CardDeck>
       </React.Fragment>
     );
